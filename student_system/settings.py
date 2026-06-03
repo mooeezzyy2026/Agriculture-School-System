67d3a6f23f4d7c71b0e93d3e1fd88118c75fc176
mooeezzyy2026 <mooeezzyy.1996@outlook.com>
2026-06-03 11:16:44 +0500

    style: registered django-unfold in settings INSTALLED_APPS

--- student_system/settings.py
+++ student_system/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['mooeezzyy2026.pythonanywhere.com', '127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    "unfold", #
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
